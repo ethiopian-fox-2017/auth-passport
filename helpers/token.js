47f8476dd081f04f3114be8727535eb36f090b0e
maulana <maulana.robert.mr@gmail.com>
2017-04-14 13:00:14 +0700

    move token to helper

--- helpers/token.js
+++ helpers/token.js
@@ -5,6 +5,7 @@ let verifyToken = (req, res, next) => {
   jwt.verify(req.headers.token, process.env.SECRET_KEY, function(err, decoded) {
     if (decoded) {
       req.decoded = decoded;
+      console.log(req.decoded);
       next();
     }else{
       res.send("Unauthorized")
